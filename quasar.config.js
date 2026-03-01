5db0048b567c123832a039500d2e4ee16142db3c
D A B JAYASEKARA <deepal.anu.com@gmail.com>
2026-03-01 11:44:48 +0530

    Add GitHub Actions deploy workflow and configure publicPath for GitHub Pages

--- quasar.config.js
+++ quasar.config.js
@@ -45,7 +45,7 @@ export default defineConfig((ctx) => {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: process.env.NODE_ENV === 'production' ? '/Lanka-Air-Force-HMS/' : '/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
